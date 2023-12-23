c4d8e4d73b36399fd029905c92382034e8e77085
Agustin <agustinescalanterojo@gmail.com>
2023-12-24 00:26:31 +0100

    styles NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,8 @@
 <template>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+  <Disclosure as="nav" class="bg-blue-900" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
           <DisclosureButton
             class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
@@ -31,7 +30,7 @@
                 :href="item.href"
                 :class="[
                   item.current
-                    ? 'bg-gray-900 text-white'
+                    ? 'text-gray-300 text-white'
                     : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                   'rounded-md px-3 py-2 text-sm font-medium',
                 ]"
@@ -44,46 +43,30 @@
         <div
           class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
-
-        <div class="ml-4 relative">
-  <select
-    v-model="selectedLanguage"
-    @change="changeLanguage"
-    class="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:ring focus:border-indigo-500 focus:bg-white focus:text-gray-900"
-  >
-  <option selected>Idioma</option>
-    <option value="es">Español</option>
-    <option value="en">English</option>
-  </select>
-  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 8l5 5 5-5z"/></svg>
-  </div>
-</div>
-
-          <!-- <button
-            type="button"
-            class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span class="absolute -inset-1.5" />
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-          </button> -->
-
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <!-- <div>
-              <MenuButton
-                class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          <div class="ml-4 relative">
+            <select
+              v-model="selectedLanguage"
+              @change="changeLanguage"
+              class="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:ring focus:border-indigo-500 focus:bg-white focus:text-gray-900"
+            >
+              <option selected>Idioma</option>
+              <option value="es">Español</option>
+              <option value="en">English</option>
+            </select>
+            <div
+              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+            >
+              <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
               >
-                <span class="absolute -inset-1.5" />
-                <span class="sr-only">Open user menu</span>
-                <img
-                  class="h-8 w-8 rounded-full"
-                  src="https://www.mintyhost.com/wp-content/uploads/2023/06/mintyhost-logotipo.svg"
-                  alt=""
-                />
-              </MenuButton>
-            </div> -->
+                <path d="M5 8l5 5 5-5z" />
+              </svg>
+            </div>
+          </div>
+
+          <Menu as="div" class="relative ml-3">
             <transition
               enter-active-class="transition ease-out duration-100"
               enter-from-class="transform opacity-0 scale-95"
@@ -95,36 +78,6 @@
               <MenuItems
                 class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
               >
-                <!-- <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Your Profile</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Settings</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="[
-                      active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
-                    ]"
-                    >Sign out</a
-                  >
-                </MenuItem> -->
               </MenuItems>
             </transition>
           </Menu>
@@ -141,7 +94,7 @@
           :href="item.href"
           :class="[
             item.current
-              ? 'bg-gray-900 text-white'
+              ? 'text-gray-300 hover:bg-gray-700 hover:text-white'
               : 'text-gray-300 hover:bg-gray-700 hover:text-white',
             'block rounded-md px-3 py-2 text-base font-medium',
           ]"
