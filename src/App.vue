7dfb213654c2367945d0753a0077fb6e54477f36
Agustin <agustinescalanterojo@gmail.com>
2023-12-23 12:33:20 +0100

    modified component navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script >
 import NavBar from './components/NavBar.vue';
+import Filters from './components/Filters.vue';
 
 export default {
     components: {
         NavBar,
+        Filters,
     },
 };
 </script>
 
 <template>
-    <NavBar />
+ <NavBar />
+ <Filters />
+   
+    
 </template>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,53 +1,157 @@
 <template>
-  <div
-    class="bg-gray-800 text-gray-100 py-3.5 px-6 shadow md:flex justifiy-between items-center"
-  >
-    <div class="flex items-center cursor-pointer">
-      <span class="text-green-500 text-xl mr-1">
-        <i class="bi bi-messenger"></i>
-      </span>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center">
+            <img
+              class="h-8 w-auto"
+              src="https://www.mintyhost.com/wp-content/uploads/2023/06/mintyhost-logotipo.svg"
+              alt="Your Company"
+            />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <button
+            type="button"
+            class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          >
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
 
-      <h1 class="text-xl">
-        <img src="https://www.mintyhost.com/wp-content/uploads/2023/06/mintyhost-logotipo.svg" alt="Minty Host Logo" class="h-8 w-auto mr-2">
-        Minty Host
-      </h1>
+          <!-- Profile dropdown -->
+          <Menu as="div" class="relative ml-3">
+            <!-- <div>
+              <MenuButton
+                class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span class="absolute -inset-1.5" />
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="h-8 w-8 rounded-full"
+                  src="https://www.mintyhost.com/wp-content/uploads/2023/06/mintyhost-logotipo.svg"
+                  alt=""
+                />
+              </MenuButton>
+            </div> -->
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Your Profile</a
+                  >
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Settings</a
+                  >
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Sign out</a
+                  >
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+      </div>
     </div>
 
-    <span
-      @click="MenuOpen()"
-      class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl"
-    >
-      <i :class="[open ? 'bi bi-x' : 'bi bi-filte-left']"></i>
-    </span>
-
-    <ul
-      class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in"
-      :class="[open ? 'left-0' : 'left-[-100%]']">
-      <li class="md:mx-4 md:my-0 my-6" v-for="link in Links">
-        <a href="link.link" class="text-xl hover:text-green-500">{{link.name}}</a>
-      </li>
-    </ul>
-  </div>
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
-<script>
-export default {
-  setup() {
-    let Links = [
-      {
-        name: "Viviendas",
-        link: "#",
-      },
-      {
-        name: "Contactos",
-        link: "#",
-      },
-      {
-        name: "Preguntas",
-        link: "#",
-      },
-    ];
-    return { Links };
-  },
-};
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Viviendas", href: "#", current: true },
+  { name: "Contacto", href: "#", current: false },
+  { name: "Preguntas", href: "#", current: false },
+];
 </script>
